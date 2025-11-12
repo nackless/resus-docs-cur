5d48e238122548cdc685e83d5d6b59bf29a47fcb
nackless <108402910+nackless@users.noreply.github.com>
2025-11-12 00:19:15 +0000

    added alt text to main image

--- resus-docs/schemaTypes/post.ts
+++ resus-docs/schemaTypes/post.ts
@@ -32,6 +32,13 @@ export default defineType({
       options: {
         hotspot: true,
       },
+      fields: [
+        {
+          name: 'alt',
+          title: 'Alternative text',
+          type: 'string',
+        }
+      ]
     }),
     defineField({
       name: 'categories',
